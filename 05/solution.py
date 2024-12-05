e26c59778136bb311addeee853fea520c1504483
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2024-12-05 19:59:22 +0200

    Reuse Part 1 code for Part 2

--- 05/solution.py
+++ 05/solution.py
@@ -3,7 +3,7 @@ with open("./input.txt") as f:
 
 ordering, updates = data[:1176], data[1177:]
 
-part1 = 0
+part1, part2 = 0, 0
 for u in updates:
     correct = True
     nums = u.split(",")
@@ -11,24 +11,11 @@ for u in updates:
         for j in range(i + 1, len(nums)):
             if f"{nums[j]}|{nums[i]}" in ordering:
                 correct = False
+                nums[j], nums[i] = nums[i], nums[j]
     if correct:
         part1 += int(nums[len(nums) // 2])
-
-print(part1)
-
-part2 = 0
-for u in updates:
-    correct = True
-    nums = u.split(",")
-    for i in range(len(nums) - 1):
-        j = i + 1
-        while j < len(nums):
-            if f"{nums[j]}|{nums[i]}" in ordering:
-                correct = False
-                nums[j], nums[i] = nums[i], nums[j]
-                j = i
-            j += 1
-    if not correct:
+    else:
         part2 += int(nums[len(nums) // 2])
 
+print(part1)
 print(part2)
